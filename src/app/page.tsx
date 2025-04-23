fb64f022ef6803aa628d5079dbf6d0aaf2f54595
yasser ramdani <139624089+IkhMoh@users.noreply.github.com>
2025-04-23 22:08:29 +0100

    code improvments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (    
-    <main>    
+    <main>      
       <section>
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 gap-4 md:grid-cols-4 md:items-center md:gap-8">
